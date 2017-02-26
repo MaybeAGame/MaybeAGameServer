83d0acf2ccc27eadbca8be8def11cf896b1b85b3
grzegorz2047 <grzegorz2047@gmail.com>
2017-02-27 00:26:47 +0100

    init.

---
+++ src/main/java/pl/grzegorz2047/maybeagame/server/Main.java
@@ -0,0 +1,12 @@
+package pl.grzegorz2047.maybeagame.server;
+
+/**
+ * Plik stworzony przez grzegorz2047 27.02.2017.
+ */
+public class Main {
+
+    public static void main(String ... args) {
+        //http://netty.io/wiki/user-guide-for-4.x.html
+    }
+
+}
